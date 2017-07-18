f1a3e7623b9e9ac4913ec8683444a7d10cb1b11e
Andy Tuttle <andytuttle12@gmail.com>
2017-07-17 21:42:50 -0400

    Practice with higher order components and redux.

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Secret Files
+            <ul>
+                <li>State Secrets</li>
+                <li>Conspiracies</li>
+                <li>Stuff your Grandma told you</li>
+            </ul>
+        </div>
+    );
+};
